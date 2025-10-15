0121d9a9e48bd26c2ca2f87bc10f43b32e5722a2
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-15 16:03:50 -0300

    Agregando menú principal y agregando submenús del proyecto

--- Entrega1.py
+++ Entrega1.py
@@ -4,7 +4,9 @@ Título:
 Fecha: 8/10/2025
 Autores: Santino Ventrice, Valentina Ubeda, Santino Traversi y Rocco Gullo
 
-Descripción: tp de algoritmia 
+Descripción: Empresa de transporte que realiza viajes con sus vehículos y choferes.
+Cada vehículo tiene como dato su costo por kilómetro. El único usuario del sistema será el que
+gestiona los viajes en la empresa.
 
 Pendientes:
 -----------------------------------------------------------------------------------------------
@@ -41,11 +43,10 @@ def main():
             print("---------------------------")
             print("MENÚ PRINCIPAL")
             print("---------------------------")
-            print("[1] Gestión de clientes")
-            print("[2] Opción 2")
-            print("[3] Opción 3")
-            print("[4] Opción 4")
-            print("[5] Opción 5")
+            print("[1] Gestión de choferes")
+            print("[2] Gestión de vehículos")
+            print("[3] Gestión de rutas")
+            print("[4] Informes")
             print("---------------------------")
             print("[0] Salir del programa")
             print("---------------------------")
@@ -68,12 +69,12 @@ def main():
                     opciones = 4
                     print()
                     print("---------------------------")
-                    print("MENÚ PRINCIPAL > MENÚ DE CLIENTES")
+                    print("MENÚ PRINCIPAL > MENÚ DE CHOFERES")
                     print("---------------------------")
-                    print("[1] Ingresar clientes")
-                    print("[2] Opción 2")
-                    print("[3] Opción 3")
-                    print("[4] Opción 4")
+                    print("[1] Ingresar choferes")
+                    print("[2] Modificar choferes")
+                    print("[3] Eliminar choferes")
+                    print("[4] Listado choferes")
                     print("---------------------------")
                     print("[0] Volver al menú anterior")
                     print("---------------------------")
@@ -106,16 +107,122 @@ def main():
 
 
         elif opcionMenuPrincipal == "2":   # Opción 2 del menú principal
-            ...
+            while True:
+                while True:
+                    opciones = 4
+                    print()
+                    print("---------------------------")
+                    print("MENÚ PRINCIPAL > MENÚ DE VEHÍCULOS")
+                    print("---------------------------")
+                    print("[1] Ingresar vehículos")
+                    print("[2] Modificar vehículos")
+                    print("[3] Eliminar vehículos")
+                    print("[4] Listado vehículos")
+                    print("---------------------------")
+                    print("[0] Volver al menú anterior")
+                    print("---------------------------")
+                    print()
+                    
+                    opcionSubmenu = input("Seleccione una opción: ")
+                    if opcionSubmenu in [str(i) for i in range(0, opciones + 1)]: # Sólo continua si se elije una opcion de menú válida
+                        break
+                    else:
+                        input("Opción inválida. Presione ENTER para volver a seleccionar.")
+                print()
+
+                if opcionSubmenu == "0": # Opción salir del submenú
+                    break # No sale del programa, sino que vuelve al menú anterior
+                
+                elif opcionSubmenu == "1":   # Opción 1 del submenú
+                    ...
+                    
+                elif opcionSubmenu == "2":   # Opción 2 del submenú
+                    ...
+                
+                elif opcionSubmenu == "3":   # Opción 3 del submenú
+                    ...
+                
+                elif opcionSubmenu == "4":   # Opción 4 del submenú
+                    ...
+
+                input("\nPresione ENTER para volver al menú.") # Pausa entre opciones
+                print("\n\n")
         
         elif opcionMenuPrincipal == "3":   # Opción 3 del menú principal
-            ...
+            while True:
+                while True:
+                    opciones = 1
+                    print()
+                    print("---------------------------")
+                    print("MENÚ PRINCIPAL > MENÚ DE RUTAS")
+                    print("---------------------------")
+                    print("[1] Registro de rutas")
+                    print("---------------------------")
+                    print("[0] Volver al menú anterior")
+                    print("---------------------------")
+                    print()
+                    
+                    opcionSubmenu = input("Seleccione una opción: ")
+                    if opcionSubmenu in [str(i) for i in range(0, opciones + 1)]: # Sólo continua si se elije una opcion de menú válida
+                        break
+                    else:
+                        input("Opción inválida. Presione ENTER para volver a seleccionar.")
+                print()
+
+                if opcionSubmenu == "0": # Opción salir del submenú
+                    break # No sale del programa, sino que vuelve al menú anterior
+                
+                elif opcionSubmenu == "1":   # Opción 1 del submenú
+                    ...
+
+                input("\nPresione ENTER para volver al menú.") # Pausa entre opciones
+                print("\n\n")
         
         elif opcionMenuPrincipal == "4":   # Opción 4 del menú principal
-            ...
+            while True:
+                while True:
+                    opciones = 5
+                    print()
+                    print("---------------------------")
+                    print("MENÚ PRINCIPAL > MENÚ DE INFORMES")
+                    print("---------------------------")
+                    print("[1] Informe viajes del mes")
+                    print("[2] Resumen mensual de rutas por vehículo")
+                    print("[3] Resumen mensual de rutas por vehículos")
+                    print("[4] Ranking choferes")
+                    print("[5] Ranking vehículos")
+                    print("---------------------------")
+                    print("[0] Volver al menú anterior")
+                    print("---------------------------")
+                    print()
+                    
+                    opcionSubmenu = input("Seleccione una opción: ")
+                    if opcionSubmenu in [str(i) for i in range(0, opciones + 1)]: # Sólo continua si se elije una opcion de menú válida
+                        break
+                    else:
+                        input("Opción inválida. Presione ENTER para volver a seleccionar.")
+                print()
 
-        elif opcionMenuPrincipal == "5":   # Opción 5 del menú principal
-            ...
+                if opcionSubmenu == "0": # Opción salir del submenú
+                    break # No sale del programa, sino que vuelve al menú anterior
+                
+                elif opcionSubmenu == "1":   # Opción 1 del submenú
+                    ...
+                    
+                elif opcionSubmenu == "2":   # Opción 2 del submenú
+                    ...
+                
+                elif opcionSubmenu == "3":   # Opción 3 del submenú
+                    ...
+                
+                elif opcionSubmenu == "4":   # Opción 4 del submenú
+                    ...
+
+                elif opcionSubmenu == "5":   # Opción 4 del submenú
+                    ...
+
+                input("\nPresione ENTER para volver al menú.") # Pausa entre opciones
+                print("\n\n")
 
         if opcionSubmenu != "0": # Pausa entre opciones. No la realiza si se vuelve de un submenú
             input("\nPresione ENTER para volver al menú.")
